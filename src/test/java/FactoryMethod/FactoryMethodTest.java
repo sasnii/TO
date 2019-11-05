4d04032c2878d7aa26c5b935e6120b53152ba9ba
sasnii <sasnii@users.noreply.github.com>
2019-11-05 21:06:34 +0100

    Adding tests to FactoryMethod

---
+++ src/test/java/FactoryMethod/FactoryMethodTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryMethod;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author kubas
+ */
+public class FactoryMethodTest {
+    
+    public FactoryMethodTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        FactoryMethod.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ src/test/java/FactoryMethod/SalamiTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FactoryMethod;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author kubas
+ */
+public class SalamiTest {
+    
+    public SalamiTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMake() {
+        System.out.println("make");
+        Salami instance = new Salami();
+        instance.make();
+        fail("The test case is a prototype.");
+    }
+    
+}
