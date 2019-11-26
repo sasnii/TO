103ea7c14484069496a46db415d0e68f4c507ada
sasnii <sasnii@users.noreply.github.com>
2019-11-26 20:01:19 +0100

    Singleton

--- src/main/java/Singleton/Singleton.java
+++ src/main/java/Singleton/Singleton.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Singleton;
 
-/**
- *
- * @author kubas
- */
 public class Singleton {
-    
+    private static Singleton obj; 
+  
+    private Singleton() {} 
+  
+    public static Singleton getInstance() 
+    { 
+        if (obj==null) 
+            obj = new Singleton(); 
+        return obj; 
+    } 
 }
