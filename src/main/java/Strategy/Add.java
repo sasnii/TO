80b694160762c18f32207b8b7e1050361cd60cdb
sasnii <sasnii@users.noreply.github.com>
2020-01-08 18:23:04 +0100

    Strategy

---
+++ src/main/java/Strategy/Add.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strategy;
+
+/**
+ *
+ * @author kubas
+ */
+public class Add implements Strategy{
+    @Override
+    public int calcIt(int a, int b){
+        return a + b;
+    }
+}
